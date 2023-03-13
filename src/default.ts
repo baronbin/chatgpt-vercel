af654e7663fea817b63548c6ba20271644d89684
baronbin <292547112@qq.com>
2023-03-13 20:37:34 +0800

    1

--- src/default.ts
+++ src/default.ts
@@ -8,4 +8,4 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 由 [OpenAI API (gpt-3.5-turbo)]和 [DEPPX](http://www.deppx.com/) 提供支持。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 开头输入<kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
